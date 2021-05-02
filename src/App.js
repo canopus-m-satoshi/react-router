8bb534a660842554e2df76606992ba9349a07f8f
Satoshi Matushima <58695418+satoshi-matsushima@users.noreply.github.com>
2021-05-03 00:30:59 +0900

    19. ネストされたページ遷移

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
 
 import { Home } from "./Home";
 import { Page1 } from "./Page1";
+import { Page1DetailA } from "./Page1DetailA";
+import { Page1DetailB } from "./Page1DetailB";
 import { Page2 } from "./Page2";
 import "./styles.css";
 
@@ -33,9 +35,22 @@ export default function App() {
         </Route>
 
         {/* page1のURLで来た場合には、page１コンポーネント表示させる */}
-        <Route path="/page1">
-          <Page1 />
-        </Route>
+        <Route
+          path="/page1"
+          render={({ match: { url } }) => (
+            <Switch>
+              <Route exact path={url}>
+                <Page1 />
+              </Route>
+              <Route path={`${url}/detailA`}>
+                <Page1DetailA />
+              </Route>
+              <Route path={`${url}/detailB`}>
+                <Page1DetailB />
+              </Route>
+            </Switch>
+          )}
+        />
 
         {/* page2のURLで来た場合には、page１コンポーネント表示させる */}
         <Route path="/page2">
