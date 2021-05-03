ec08621623861e352426ce56ec442e9d7e1cf915
Satoshi Matushima <58695418+satoshi-matsushima@users.noreply.github.com>
2021-05-03 15:05:03 +0900

    22. クエリパラメータを扱う https://www.udemy.com/course/react_stepup/learn/lecture/24823424#content

--- src/Page2.jsx
+++ src/Page2.jsx
@@ -4,7 +4,12 @@ export const Page2 = () => {
   return (
     <div>
       <h1>Page2ページです</h1>
+
+      {/* URLパラメータ */}
       <Link to="/page2/100">URL Parameter</Link>
+      <br />
+      {/* クエリパラメータ：URL末尾の?以下のもの */}
+      <Link to="/page2/100?name=hogehoge">Query Parameter</Link>
     </div>
   );
 };
--- src/UrlParameter.jsx
+++ src/UrlParameter.jsx
@@ -1,12 +1,23 @@
-//  useParamsフックスでURLパラメータを扱う
-import { useParams } from "react-router-dom";
+//  useParamsフックス：URLパラメータを扱う
+//  useLocationフックス：クエリパラメータを扱う
+
+import { useParams, useLocation } from "react-router-dom";
 
 export const UrlParameter = () => {
   const { id } = useParams();
+
+  // URLの？以降の文字列を取得→取得したserachをURLSearchParamsの引数に渡→
+  const { search } = useLocation();
+
+  // URLSearchParams：クエリパラメータないで使える便利なメソッド等を提供
+  const query = new URLSearchParams(search);
+  console.log(query);
+
   return (
     <div>
       <h1>UrlParameterページです</h1>
-      <p>UrlParameter is {id}</p>
+      <p>パラメータ は {id}</p>
+      <p>クエリパラメータ は {query.get("name")}</p>
     </div>
   );
 };
