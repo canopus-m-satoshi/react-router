eec467dfd2e8a3e70370193a819ed7e9346d9a6f
Satoshi Matushima <58695418+satoshi-matsushima@users.noreply.github.com>
2021-05-03 15:29:16 +0900

    24. Linkを使わないページ遷移 https://www.udemy.com/course/react_stepup/learn/lecture/24823434#content

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,9 +1,14 @@
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 export const Page1 = () => {
   // arrの情報を持ったままdetailAにページ遷移
   const arr = [...Array(100).keys()];
-  console.log(arr);
+
+  // linkタグを使わず、ボタンをクリックしたらページ遷移する方法(useHistory)
+  const history = useHistory();
+
+  // pushメソッドで引数に遷移先パスを渡す
+  const onClickDetailA = () => history.push("/page1/detailA");
 
   return (
     <div>
@@ -11,6 +16,10 @@ export const Page1 = () => {
       <Link to={{ pathname: "/page1/detailA", state: arr }}>DetailA</Link>
       <br />
       <Link to="/page1/detailB">DetailB</Link>
+      <br />
+
+      {/* linkタグを使わず、ボタンをクリックしたらページ遷移する方法(useHistory) */}
+      <button onClick={onClickDetailA}>DetailA</button>
     </div>
   );
 };
